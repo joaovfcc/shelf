d799e22395f59e8ee88836fc5245f0301d28d26d
João Vitor Farias <vitorfchaves1@gmail.com>
2025-08-25 22:34:41 -0300

    Project created

---
+++ src/main/java/com/soloproject/shelf/ShelfApplication.java
@@ -0,0 +1,13 @@
+package com.soloproject.shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soloproject/shelf/ShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soloproject.shelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
