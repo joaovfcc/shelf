64bf9e7a68257650a7d74934e78b85ffdf06955c
Joao Vitor Farias <vitorfchaves1@gmail.com>
2025-08-27 18:50:42 -0300

    Criacao DTO

---
+++ src/main/java/com/soloproject/shelf/model/dto/GameMinDTO.java
@@ -0,0 +1,43 @@
+package com.soloproject.shelf.model.dto;
+
+import com.soloproject.shelf.model.entitites.Game;
+import jakarta.persistence.Column;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String tittle;
+    private Integer year;
+    private String imageUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {}
+
+    public GameMinDTO(Game entity) {
+        id = entity.getID();
+        tittle = entity.getTitle();
+        year = entity.getYear();
+        imageUrl = entity.getImageUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
